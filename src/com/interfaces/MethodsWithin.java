c7dc002c9fc25f0ac95470d76b25018919156fe3
Sushil Sridhar <sushilsridhar16@gmail.com>
2019-11-07 22:07:00 +0530

    Methods inside interface

---
+++ src/com/interfaces/MethodsWithin.java
@@ -0,0 +1,30 @@
+package com.interfaces;
+
+interface Phone {
+
+    void call();
+
+    default void message() {
+        System.out.println("sent");
+    }
+
+    static void connectToNetwork() {
+        System.out.println("connect to network");
+    }
+}
+
+
+class Iphone implements Phone {
+    public void call() {
+        System.out.println("Hello World");
+    }
+}
+
+public class MethodsWithin {
+
+    public static void main(String[] args) {
+        Phone phone = new Iphone();
+        phone.call();
+        phone.message();
+    }
+}
